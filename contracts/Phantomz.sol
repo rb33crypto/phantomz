fcdb650189ab21d5df6ca464ce92f48f0178d561
rb33crypto <rb33crypto@gmail.com>
2024-08-20 16:23:45 -0700

    still working...

--- contracts/Phantomz.sol
+++ contracts/Phantomz.sol
@@ -10,6 +10,17 @@ contract Token{
 	uint public totalSupply;
 
 	mapping(address => uint256) public balanceOf;
+	mapping(address => mapping(address => uint256)) public allowance;
+
+	event Transfer( 
+		address indexed from,
+		address indexed to,
+		uint256 value);
+
+	event Approval(
+		address indexed owner,
+		address indexed spender,
+		uint256 value);
 
 	constructor(
 		string memory _name, 
@@ -21,4 +32,77 @@ contract Token{
 		totalSupply = _totalSupply*(10**decimals);
 		balanceOf[msg.sender] = totalSupply;
 	}
+
+	function _transfer(
+		address _from,
+		address _to,
+		uint256 _value) 
+	internal{
+		require(_to != address(0));
+
+		balanceOf[_from] -= _value;
+		balanceOf[_to ] += _value;
+
+		emit Transfer(_from, _to, _value);
+	}
+
+	function transfer(
+		address _to,
+		uint256 _value)
+	public 
+	returns( bool success){
+		require(balanceOf[msg.sender] >= _value);
+
+		_transfer(msg.sender, _to, _value);
+
+		return true;
+	}
+
+	function transferFrom(
+		address _from,
+		address _to,
+		uint256 _value)
+	public 
+	returns(bool success){
+		require(_value <= balanceOf[_from]);
+		require(_value <= allowance[_from][msg.sender]);
+
+		allowance[_from][msg.sender] -= _value;
+
+		_transfer(_from,_to,_value);
+
+		return true;
+	}
+
+	function approve( 
+		address _spender,
+		uint256 _value)
+	public 
+	returns (bool success){
+		require(_spender != address(0));
+
+		allowance[msg.sender][_spender] = _value;
+
+		emit Approval(msg.sender, _spender, _value);
+
+		return true;
+	}
+	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- test/Phantomz.js
+++ test/Phantomz.js
@@ -44,4 +44,60 @@ describe('Token', () =>{
 			expect(await token.balanceOf(deployer.address)).to.equal(totalSupply)
 		})
 	})
+
+	describe('Sending Tokens', () => {
+		let amount, transaction, result
+
+		describe('Success', async () => {
+			beforeEach( async () => {
+				amount = tokens(100)
+				transaction = await token.connect(deployer).transfer(receiver.address, amount)
+				result = await transaction.wait()
+			})
+
+			it('transfers token balances', async () => {
+				expect(await token.balanceOf(deployer.address)).to.equal(tokens(999900))
+				expect(await token.balanceOf(receiver.address)).to.equal(amount)
+			})
+
+			it('emits transfer event', async () => {
+				const event = result.events[0]
+				expect(event.event).to.equal('Transfer')
+
+				const args = event.args
+				expect(args.from).to.equal(deployer.address)
+				expect(args.to).to.equal(receiver.address)
+				expect(args.value).to.equal(amount)
+			})
+		})
+
+		describe('Failure', () => {
+			it('rejects insuffcient balance' async () => {
+				const invalidAmount = tokens(1000000000000)
+				await expect(token.connect(deployer).tranfer(receiver.address, invalidAmount)).to.be.reverted
+			})
+
+			it('rejects invalid reciepient' async () => {
+				const amount = tokens(100)
+				await expect(token.connect(deployer).tranfer('0x0000000000000000000000000000000000000000', amount)).to.be.reverted
+			})
+
+		})
+	})
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
